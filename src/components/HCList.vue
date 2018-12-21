77ea4d4b133522f800fdd8699cec8857199443e5
gales34 <gales34@zuminternet.com>
2018-12-21 17:40:23 +0900

    little change

--- src/components/HCList.vue
+++ src/components/HCList.vue
@@ -1,18 +1,18 @@
 <template>
-  <v-container>
+  <v-container align-center justify-center row fluid>
     <ul>
       <li v-for="(member, index) in this.$store.state.memberList" v-bind:key="index" class="shadow">
-        <v-layout class="ma-0 pa-0">
+        <v-layout class="ma-0 pa-0" xs5>
           <v-flex class="mx-1">
             <v-text-field
                 height="24px"
                 hide-details v-model.number="member.grade" label="학년" outline type="number"></v-text-field>
           </v-flex>
-          <v-flex class="mx-1">
+          <v-flex class="mx-1" xs5>
             <v-text-field
                 hide-details v-model.number="member.count" label="명" outline type="number"></v-text-field>
           </v-flex>
-          <v-flex align-self-center>
+          <v-flex align-self-center xs1 style="text-align: center;">
             <v-icon x-large color="green darken-2" @click="removeMember(index)">
               delete
             </v-icon>
