854a2384de6b3fcfb4cf9afaae8aedcd1181bc50
gales34 <gales34@zuminternet.com>
2018-12-21 11:14:50 +0900

    add vuetify, build

--- src/App.vue
+++ src/App.vue
@@ -1,78 +1,38 @@
 <template>
-  <div id="app">
-    <HCHeader></HCHeader>
-    <HCDefaultInput></HCDefaultInput>
-    <HCList></HCList>
-    <HCInput></HCInput>
-    <HCFooter></HCFooter>
-  </div>
+  <v-app>
+    <v-toolbar app>
+      <v-toolbar-title class="headline text-uppercase">
+        <span>Vuetify</span>
+        <span class="font-weight-light">MATERIAL DESIGN</span>
+      </v-toolbar-title>
+      <v-spacer></v-spacer>
+      <v-btn
+        flat
+        href="https://github.com/vuetifyjs/vuetify/releases/latest"
+        target="_blank"
+      >
+        <span class="mr-2">Latest Release</span>
+      </v-btn>
+    </v-toolbar>
+
+    <v-content>
+      <HelloWorld/>
+    </v-content>
+  </v-app>
 </template>
 
 <script>
-import HCHeader from './components/HCHeader.vue'
-import HCDefaultInput from './components/HCDefaultInput.vue'
-import HCInput from './components/HCInput.vue'
-import HCList from './components/HCList.vue'
-import HCFooter from './components/HCFooter.vue'
+import HelloWorld from './components/HelloWorld'
 
 export default {
-  name: 'app',
+  name: 'App',
   components: {
-    HCHeader,
-    HCDefaultInput,
-    HCInput,
-    HCList,
-    HCFooter
+    HelloWorld
+  },
+  data () {
+    return {
+      //
+    }
   }
 }
 </script>
-
-<style>
-/*#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}*/
-
-body {
-  text-align: center;
-  background-color: #f6f6f6;
-}
-
-input {
-  border-style: groove;
-  width: 200px;
-}
-
-button {
-  border-style: groove;
-}
-
-.shadow {
-  box-shadow: 5px 10px 10px rgba(0, 0, 0, 0.03);
-}
-
-.inputBox {
-  background: white;
-  height: 20px;
-  line-height: 20px;
-  width: 25%;
-  border-radius: 5px;
-}
-
-.inputBox input {
-  border-style: none;
-  font-size: 0.9rem;
-}
-
-.addContainer {
-  float: right;
-  background: linear-gradient(to right, #6478fb, #8763fb);
-  display: block;
-  width: 3rem;
-  border-radius: 0 5px 5px 0;
-}
-</style>
